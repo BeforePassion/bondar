ccb2d4aac18170d2957ccf08934ca9043f6d81e0
bolonge <qhsghd5357@gmail.com>
2022-02-25 15:01:02 +0900

    edit image service & test service

--- userprofile/services/profile_service.py
+++ userprofile/services/profile_service.py
@@ -18,6 +18,15 @@ async def async_create_image(user_id: int, original_image_url: str, nst_image_ur
     return cast(UserProfile, result)
 
 
-def editUser(user_id: int) -> None:
-    # User.objects.filter(id=user_id).get()
-    return
+def edit_image(user_id: int, original_image_url: str, nst_image_url: str) -> UserProfile:
+    UserModel.objects.filter(id=user_id).get()
+    userImage = UserProfile.objects.filter(user_id=user_id).get()
+    userImage.original_image_url = original_image_url
+    userImage.nst_image_url = nst_image_url
+    userImage.save(update_fields=['original_image_url', 'nst_image_url'])
+    return userImage
+
+
+async def async_edit_image(user_id: int, original_image_url: str, nst_image_url: str) -> UserProfile:
+    result = await sync_to_async(edit_image)(user_id, original_image_url, nst_image_url)
+    return cast(UserProfile, result)
