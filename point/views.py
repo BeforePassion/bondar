5e068c467817e34af0487c9b50f3544a5c7b7d7e
hojin <hojin9car@gmail.com>
2022-02-25 12:27:30 +0900

    edit view.py 임시저장

--- point/views.py
+++ point/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from ninja import NinjaAPI
+
+from .models import PointHistory
+
+api = NinjaAPI()
 
 
-def test(request):
-
-    return render(request, "point/test.html")
+@api.get("/point")
+def test(request) -> None:
+    template_name = "point/test.html"
+    return render(template_name)
+    # point_list = PointHistory.objects.filter()
