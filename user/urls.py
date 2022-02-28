ef882e5507ca873cb4e2b8040c7ae8c84b446fa6
Elijahrong <nhkim5760@gmail.com>
2022-02-28 17:55:37 +0900

    example file(not real)

--- user/urls.py
+++ user/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.test, name="test"),
+    path('sign-up/', views.sign_up_view, name='sign-up'),
+    path('sign-in/', views.sign_in_view, name='sign-in'),
+    path('logout/', views.logout, name='logout'),
 ]
