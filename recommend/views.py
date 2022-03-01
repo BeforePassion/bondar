56b78f900528e2339168c707b0daea8b16dc1d57
Hosio123 <southpaw@kakao.com>
2022-03-01 16:31:40 +0900

    like model

--- recommend/views.py
+++ recommend/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from user.models import UserModel
+
+
+def user_view(request):
+    if request.method == 'GET':
+        user_list = UserModel.objects.all().exclude()
