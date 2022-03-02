a4c905ccefa1353306d2fe6468ce2c8ab27639c3
bolonge <qhsghd5357@gmail.com>
2022-03-02 17:45:22 +0900

    update user profile

--- userprofile/apis/v1/schemas/__init__.py
+++ userprofile/apis/v1/schemas/__init__.py
@@ -1,2 +1,4 @@
 from .file_url_request import FileUrlRequest as FileUrlRequest
 from .file_url_response import FileUrlResponse as FileUrlResponse
+from .profile_user_request import ProfileRequest as ProfileRequest
+from .profile_user_response import UserSchema as UserSchema
