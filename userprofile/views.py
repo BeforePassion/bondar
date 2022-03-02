77a94928639739df58def8ad0b33c32283677457
bolonge <qhsghd5357@gmail.com>
2022-03-02 23:47:56 +0900

    regitster update

--- userprofile/views.py
+++ userprofile/views.py
@@ -1,21 +1,26 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect, render
-from django.contrib.auth.decorators import login_required
 
 
 def profile(request: HttpRequest) -> HttpResponse:
     if request.method == 'GET':
-        user = request.user.is_authenticated
-        if user:
-            return render(request, "profile/update_profile.html")
+        user = request.user
+        if user.is_authenticated:
+            if user.invalid_user:
+                return render(request, "profile/update_profile.html")
+            else:
+                return redirect('/register')
         else:
             return redirect('/sign-in')
 
 
 def image_profile(request: HttpRequest) -> HttpResponse:
     if request.method == 'GET':
-        user = request.user.is_authenticated
-        if user:
-            return render(request, "profile/update_image.html")
+        user = request.user
+        if user.is_authenticated:
+            if user.invalid_user:
+                return render(request, "profile/update_image.html")
+            else:
+                return redirect('/register')
         else:
             return redirect('/sign-in')
