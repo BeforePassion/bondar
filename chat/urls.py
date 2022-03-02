b1807deddce86b7c3c0a40b575dd44672d6f1460
Github <weeds1590@gmail.com>
2022-03-02 22:30:21 +0900

    add 서로 좋아하는 유저 채팅방 create api

--- chat/urls.py
+++ chat/urls.py
@@ -5,4 +5,5 @@ from chat import views
 urlpatterns = [
     path("", views.chat_view, name="chat"),  # /chat/으로 넘어오면 chat_view 함수 실행
     path("<str:room_name>/", views.room_view, name="room"),  # /chat/room_number/ 으로 넘어오면 room 함수 실행
+    path("api/<int:user_id>", views.api_create_room, name="api_create_room"),
 ]
