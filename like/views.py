14b5f195b68865ff1b6b3cb6bc059c2afbf14a7d
Hosio123 <southpaw@kakao.com>
2022-03-02 19:26:49 +0900

    delete comment

--- like/views.py
+++ like/views.py
@@ -1,20 +1,9 @@
-from random import random
-
 from django.shortcuts import render, redirect
-
-from userprofile.models import UserProfile
 from user.models import UserModel
 from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
-# @login_required
-# def user_list(request):
-#     if request.method == 'GET':
-#         random_num = random.randint(1, UserModel.objects.all.count())
-#         user = UserModel.objects.exclude(id=random_num)
-#         user_img = UserProfile.objects.filter(user_id=user.id)
-#         return render(request, 'main.html', {'user': user, 'user_img': user_img})
 @login_required
 def user_view(request):
     if request.method == 'GET':
@@ -37,25 +26,3 @@ def user_like(request, id):
     click_user = UserModel.objects.get(id=id)
     click_user.friends.add(request.user)
     return redirect('/main')
-
-
-
-
-
-
-# def unlike
-# if me in click_user.friends.all():
-#     click_user.friends.remove(request.user)
-
-# @login_required
-# def user_follow(request):
-#     if request.method == 'POST':
-#         me = request.user
-#         click_user = UserModel.objects.get(id=me.id)
-#         if me in click_user.followee.all():
-#             click_user.friend.remove(request.user)
-#         else:
-#             click_user.friend.add(request.user)
-#         return redirect('/main')
-#     elif request.method == 'GET':
-#         return render(request, 'main.html')
